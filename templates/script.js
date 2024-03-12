97bc70af0e1f0b5d087e15fa5ff206b8e2c7c492
ghostbusters <eeshashafqat2@gmail.com>
2024-03-12 23:38:31 +0500

    modified app.py to handle database

---
+++ templates/script.js
@@ -0,0 +1,11 @@
+function addTask() {
+    var taskInput = document.getElementById('taskInput');
+    var taskList = document.getElementById('taskList');
+
+    if (taskInput.value !== '') {
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(taskInput.value));
+        taskList.appendChild(li);
+        taskInput.value = '';
+    }
+}
